8337e8b460edc28f2d64ca20159fa7384f32af9f
MTeeeee <mete.ardic@gmail.com>
2023-12-06 10:34:19 +0100

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import unittest
-
 def addition(zahl_1, zahl_2):
     return zahl_1 + zahl_2
 
-##
-# Eine Funktion die Text einließt und in Großbuchstaben wieder ausgibt:
 def upper_case(string):
     return string.upper()
-##
+
 
 
